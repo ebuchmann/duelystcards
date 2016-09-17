1a2694b4102e436ecb52a6e463597b3d1dfd2ab9
Eric Buchmann <me@ebuchmann.com>
2016-09-17 16:00:25 -0700

    added domtoimage to test saving as an image

--- src/components/DeckList.vue
+++ src/components/DeckList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="deck-list">
+  <div class="deck-list" ref="test">
     <mana-curve></mana-curve>
     <deck-counts></deck-counts>
     <deck-general></deck-general>
@@ -9,6 +9,7 @@
     <div class="cards">
       <deck-card v-for="card in cardList" :card="card"></deck-card>
     </div>
+    <button @click="save()">Save Test</button>
   </div>
 </template>
 
@@ -17,11 +18,22 @@
   import DeckCounts from './DeckCounts'
   import DeckGeneral from './DeckGeneral'
   import DeckCard from './DeckCard'
+  import domtoimage from 'dom-to-image';
 
   export default {
     computed: {
       cardList() {
         return this.$store.state.deck.cards.sort((a, b) => a.name > b.name).sort((a, b) => a.cost > b.cost)
+      },
+    },
+
+    methods: {
+      async save () {
+        const dataUrl = await domtoimage.toJpeg(this.$refs.test)
+        const link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
       },
     },
 
