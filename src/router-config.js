0c584f89e575c1659a08163babd07573b3a3ef27
Eric Buchmann <me@ebuchmann.com>
2016-09-27 16:03:14 -0700

    changed default route

--- src/router-config.js
+++ src/router-config.js
@@ -5,7 +5,7 @@ import Deck from 'pages/Deck'
 
 export const router = new VueRouter({
   mode: 'history',
-  base: __dirname,
+  base: 'deckbuilder',
   routes: [
     { path: '/', component: Home },
     { path: '/deck', component: Home },
