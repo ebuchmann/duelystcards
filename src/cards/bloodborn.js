b8ac65805a0dc33f3eb1322f465c33fe17b53c3b
Eric Buchmann <me@ebuchmann.com>
2016-12-12 18:03:33 -0800

    adding in known bloodborn cards

---
+++ src/cards/bloodborn.js
@@ -0,0 +1,35 @@
+const bloodborn = [
+  // Abyssian
+  { id: null, name: 'Furosa', set: 'bloodborn', type: 'minion', cost: 1, rarity: 'common', attack: 1, health: 2, faction: 'abyssian', text: '<b>Blood Surge:</b> Friendly Wraithlings gain +1/+1.' },
+  { id: null, name: 'Punish', set: 'bloodborn', type: 'spell', cost: 1, rarity: 'rare', faction: 'abyssian', text: 'Destroy a damaged minion.' },
+  { id: null, name: 'Necrotic Sphere', set: 'bloodborn', type: 'spell', cost: 6, rarity: 'epic', faction: 'abyssian', text: 'Destroy ALL minions nearby your General and summon Wraithlings in their place.' },
+  { id: null, name: 'Horror Burster', set: 'bloodborn', type: 'spell', cost: 3, rarity: 'rare', attack: 4, health: 1, faction: 'abyssian', text: '<b>Dying Wish:</b> Transform a random friendly minion into a 6/6 Horror.' },
+
+  // Lyonar
+  { id: null, name: 'Scintilla', set: 'bloodborn', type: 'minion', cost: 3, rarity: 'common', attack: 3, health: 4, faction: 'lyonar', text: '<b>Blood Surge:</b> Restore 3 Health to your General.' },
+  { id: null, name: 'Excelsious', set: 'bloodborn', type: 'minion', cost: 8, rarity: 'legendary', attack: 6, health: 6, faction: 'lyonar', text: '<b>Provoke</b>, <b>Celerity</b> This minion gains +1/+1 for each time you healed anything this game.' },
+  { id: null, name: 'Sunbreaker', set: 'bloodborn', type: 'minion', cost: 4, rarity: 'rare', attack: 2, health: 4, faction: 'lyonar', text: '<b>Forcefield</b> <br>Your Bloodborn Spell is Tempest.' },
+  { id: null, name: 'Trinity Oath', set: 'bloodborn', type: 'spell', cost: 4, rarity: 'epic', faction: 'lyonar', text: 'Draw 3 cards and restore 3 Health to your General.' },
+
+  // Magmar
+  { id: null, name: 'Drogon', set: 'bloodborn', type: 'minion', cost: 4, rarity: 'legendary', attack: 5, health: 4, faction: 'magmar', text: `<b>Blood Surge:</b> Double your General's Attack this turn.` },
+  { id: null, name: 'Tectonic Spikes', set: 'bloodborn', type: 'spell', cost: 3, rarity: 'rare', faction: 'magmar', text: 'Both players draw 3 cards. Deal 3 damage to both Generals.' },
+  { id: null, name: 'Thraex', set: 'bloodborn', type: 'minion', cost: 3, rarity: 'common', attacK: 2, health: 4, faction: 'magmar', text: '<b>Blood Surge:</b> All friendly minions gain +1 Attack (including itself).' },
+
+  // Songhai
+  { id: null, name: 'Twilight Fox', set: 'bloodborn', type: 'minion', cost: 3, rarity: 'rare', attack: 3, health: 3, faction: 'songhai', text: '<b>Blood Surge:</b> Teleport a random enemy to the space behind your General.' },
+  { id: null, name: 'Geomancer', set: 'bloodborn', type: 'minion', cost: 5, rarity: 'rare', attack: 5, health: 4, faction: 'songhai', text: '<b>Opening Gambit:</b> Your Bloodborn Spell is Phoenix Fire.' },
+  { id: null, name: 'Whiplash', set: 'bloodborn', type: 'minion', cost: 3, rarity: 'common', attack: 4, health: 3, faction: 'songhai', text: '<b>Blood Surge:</b> Deal 2 damage to the enemy General.' },
+
+  // Vanar
+  { id: null, name: 'Concealing Shroud', set: 'bloodborn', type: 'spell', cost: 2, rarity: 'rare', faction: 'vanar', text: 'Your General takes no damage until your next turn.' },
+  { id: null, name: 'Grandmaster Embla', set: 'bloodborn', type: 'minion', cost: 8, rarity: 'legendary', attack: 5, health: 5, faction: 'vanar', text: '<b>Opening Gambit:</b> Surround the enemy General with random Walls.' },
+
+  // Vetruvian
+  { id: null, name: 'Stone to Spears', set: 'bloodborn', type: 'spell', cost: 1, rarity: 'rare', faction: 'vetruvian', text: 'A friendly Obelysk gains +3 Attack and may move and attack this turn if able.' },
+  { id: null, name: 'Zephyr', set: 'bloodborn', type: 'minion', cost: 3, rarity: 'common', attack: 3, health: 3, faction: 'vetruvian', text: '<b>Blood Surge:</b> Your General gains Frenzy this turn.' },
+
+  // Neutral
+  { id: null, name: 'Sanguinar', set: 'bloodborn', type: 'minion', cost: 4, rarity: 'rare', attack: 5, health: 4, faction: 'neutral', text: 'Your Bloodborn Spell costs 1 less to activate.' },
+  { id: null, name: 'Meltdown', set: 'bloodborn', type: 'minion', cost: 7, rarity: 'legendary', attack: 7, health: 7, faction: 'neutral', text: '<b>Blood Surge:</b> Deal 7 damage to a random enemy.' },
+]
