08d10cfba50f2c8fa017bbd7bfd50d7fe21f7c14
Eric Buchmann <me@ebuchmann.com>
2016-12-17 11:25:16 -0800

    chart

--- src/components/gauntlet/RarityPieChart.vue
+++ src/components/gauntlet/RarityPieChart.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="rarity-pie-chart">
-    PIE CHART {{ basic }} - {{ common }}
     <canvas ref="chart" width="200" height="200"></canvas>
   </div>
 </template>
@@ -31,38 +30,48 @@
       },
     },
 
+    methods: {
+      generateChart() {
+        new Chart(this.$refs.chart, {
+          type: 'doughnut',
+          data: {
+            labels: [
+              'Basic',
+              'Common',
+              'Rare',
+              'Epic',
+              'Legendary',
+            ],
+            datasets: [
+              {
+                data: [this.basic, this.common, this.rare, this.epic, this.legendary],
+                backgroundColor: [
+                  '#9b9999',
+                  '#c6c6c6',
+                  '#00bfff',
+                  '#c921dd',
+                  '#ff911a',
+                ],
+                hoverBackgroundColor: [
+                  '#9b9999',
+                  '#c6c6c6',
+                  '#00bfff',
+                  '#c921dd',
+                  '#ff911a',
+                ]
+              }]
+          }
+        })
+      },
+    },
+
+    watch: {
+      '$route': 'generateChart',
+    },
+
     mounted() {
-      new Chart(this.$refs.chart, {
-        type: 'doughnut',
-        data: {
-          labels: [
-            'Basic',
-            'Common',
-            'Rare',
-            'Epic',
-            'Legendary',
-          ],
-          datasets: [
-            {
-              data: [this.basic, this.common, this.rare, this.epic, this.legendary],
-              backgroundColor: [
-                '#9b9999',
-                '#c6c6c6',
-                '#00bfff',
-                '#c921dd',
-                '#ff911a',
-              ],
-              hoverBackgroundColor: [
-                '#9b9999',
-                '#c6c6c6',
-                '#00bfff',
-                '#c921dd',
-                '#ff911a',
-              ]
-            }]
-        }
-      })
-    }
+      this.generateChart()
+    },
   }
 </script>
 
