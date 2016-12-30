37088c8c985e484b43d7393d9a76a4fa6c495d9a
Eric Buchmann <me@ebuchmann.com>
2016-12-29 16:29:36 -0800

    deck landing page

--- src/pages/Deck.vue
+++ src/pages/Deck.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="gauntlet-page">
-    <h1>Gaunlet landing</h1>
+    <div class="no-decks">
+      <div class="left">
+        <img src="../assets/images/emote_vaath_confused.png" />
+      </div>
+      <div class="right">
+        <h2>Duelyst Deck Tracker</h2>
+        <p>To begin automatically tracking decks you must have <a @click="toggleProperty('loginModal')">an account</a>. Once logged in you will see instructions on how to start tracking your own decks. Later this page will be expanded to feature top ranking decks and other community decks.</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapActions, mapState } from 'vuex'
 
   export default {
+    methods: {
+      ...mapActions(['toggleProperty']),
+    },
+
     computed: {
       ...mapState({
         user: ({ user }) => user.user,
       }),
+    },
+
+    watch: {
+      user() {
+        if (this.user) this.$router.push({ name: 'deck-overview', params: { username: this.user.username } })
+      },
     },
 
     beforeRouteEnter (to, from, next) {
